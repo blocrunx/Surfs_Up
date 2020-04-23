279bd84835d45312c0909191ec58efe7558f1d4b
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-04-22 20:24:22 -0400

    routes added to flask app

--- app.py
+++ app.py
@@ -1,5 +1,84 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+
+@app.route("/")
+
+def welcome():
+    return('''
+    Welcome to the Climate Analysis API!<br>
+    Available Routes:<br>
+    /api/v1.0/precipitation<br>
+    /api/v1.0/stations<br>
+    /api/v1.0/tobs<br>
+    /api/v1.0/temp/start/end<br>
+    ''')
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+	filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations)
+
+# Create temperature observations route
+@app.route('/api/v1.0/tobs')
+# Create tobs function
+def temp_monthly():
+    # Add code that calculates the date one year ago from the most recent date in db
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days = 365)
+    # Query the primary station for all the tobs from the previous year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+            filter(Measurement.date >= prev_year).all()
+    # Unravel results into a one-dimensional array and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(results)
+
+# Create statistics route
+@app.route('/api/v1.0/temp/<start>')
+@app.route('/api/v1.0/temp/<start>/<end>')
+# Create stats function with start and end parameters
+def stats(start = None, end = None):
+    # Create list with min, max, and avg
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    # Add if-not statement, the * indicates there are multiple results for this query
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+    # Create query to get our stats data
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    # Unravel results and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+if __name__ == '__main__':
+    app.run(debug=True)
